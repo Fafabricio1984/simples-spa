f4c980a784d2367d71cd95aabb3666bb51e61963
Fafabricio1984 <fafabricio1984@gmail.com>
2021-09-18 09:50:09 -0300

    criado constantes para os elementos JS

---
+++ js/eventos.js
@@ -0,0 +1,4 @@
+const BTN-CADASTRAR = document.getElementById('btn-cadastrar');
+const BTN-LISTAR = document.getElementById('btn-listar');
+const SECTION_CADASTRAR = document.getElementById('section-cadastrar');
+const SECTION_LISTAR = document.getElementById('section-listar');
