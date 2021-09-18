544880103694b3c1088a7f1f097807ae0070e620
Fafabricio1984 <fafabricio1984@gmail.com>
2021-09-18 10:16:18 -0300

    ocultando sections com JS

--- js/eventos.js
+++ js/eventos.js
@@ -1,4 +1,14 @@
-const BTN-CADASTRAR = document.getElementById('btn-cadastrar');
-const BTN-LISTAR = document.getElementById('btn-listar');
+const BTN_CADASTRAR = document.getElementById('btn-cadastrar');
+const BTN_LISTAR = document.getElementById('btn-listar');
 const SECTION_CADASTRAR = document.getElementById('section-cadastrar');
 const SECTION_LISTAR = document.getElementById('section-listar');
+
+BTN_LISTAR.addEventListener('click', function () {
+  SECTION_CADASTRAR.style.display = 'none';
+  SECTION_LISTAR.style.display = 'block';
+});
+
+BTN_CADASTRAR.addEventListener('click', function () {
+  SECTION_LISTAR.style.display = 'none';
+  SECTION_CADASTRAR.style.display = 'block';
+});
